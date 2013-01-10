b48db194cc99e74dadefbd15597489424b653c66
林宇 <linyupark@gmail.com>
2013-01-10 09:39:39 +0800

    first commit

---
+++ nodenv/coffee.cmd
@@ -0,0 +1,6 @@
+:: Created by npm, please don't edit manually.
+@IF EXIST "%~dp0\node.exe" (
+  "%~dp0\node.exe"  "%~dp0\node_modules\coffee-script\bin\coffee" %*
+) ELSE (
+  node  "%~dp0\node_modules\coffee-script\bin\coffee" %*
+)
